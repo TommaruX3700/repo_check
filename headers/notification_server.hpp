bf22f0cd20462e4924219bc330c37bb730c6420c
TommaruX3700 <tommasomaruzzo@gmail.com>
2025-02-04 17:56:45 +0100

    Enhance NotificationServer: add coroutine and resumable support, switch to shared_mutex for thread safety, and implement sendQueue method for batch notification handling

--- headers/notification_server.hpp
+++ headers/notification_server.hpp
@@ -4,7 +4,7 @@
 #include <queue>
 
 #include <thread>
-#include <mutex>
+#include <shared_mutex>
 #include <condition_variable>
 // #include <functional>
 
@@ -13,39 +13,48 @@
 /*
     TODO:
         - Creare un tread "worker" nel costruttore
-            - Se "start" non è a true, raccogli i log in queue ma non puoi inviarli
+        - Se "start" non è a true, raccogli i log in queue ma non puoi inviarli
         - creare un distruttore adeguato
         ok - fare una queue per i messaggi
         - fare metodo ESPOSTO al main send_notification() per aggiungere alla queue il messaggio
         - mutex su queue
         - condition_variable per triggerare il thread e leggere la queue
         - thread accede alla queue per inviare i messaggi
-
 */
 
 class NotificationServer
 {
 private:
-    bool started = false;
+    
     std::string log_file_path, mqtt_address;
     
-    // Minimum_notification_level defines what is the minim level required to notifications
-    // for beign published
+    // Minimum_notification_level defines what is the minim level required to notifications for beign published
     NotificationLevels minimum_notification_level;
+
     // Here are stored all the notifications 
     std::queue<Notification>notification_queue;
 
-    std::mutex worker_mutex;
+    // manages access to queue
+    std::shared_mutex worker_mutex;
+    
+    // manages the worker
+    bool started = false;
     std::condition_variable work_todo;
     
-    std::thread worker;
     OutputCodes do_work();
-    
+    std::thread worker;
+
 public:
-    NotificationServer(std::string _log_file_path, std::string _mqtt_address, NotificationLevels _notification_level);
+    NotificationServer(std::string _log_file_path, std::string _mqtt_address, NotificationLevels _notification_level)
+        : worker(&NotificationServer::do_work, this) {}
     ~NotificationServer();
 
     void start();
     OutputCodes send(Notification message);
+    OutputCodes sendQueue(std::queue<Notification>* messages_queue);
+    // send set of message
+
+    void setLogFilePath(std::string log_file);
+    void setMqttAddress(std::string mqtt);
 };
 
--- src/notification_server.cpp
+++ src/notification_server.cpp
@@ -3,6 +3,7 @@
 NotificationServer::NotificationServer(std::string _log_file_path, std::string _mqtt_address, NotificationLevels _notification_level)
     : log_file_path { _log_file_path }, mqtt_address { _mqtt_address }, minimum_notification_level { _notification_level }
 {
+    
 }
 
 NotificationServer::~NotificationServer()
@@ -12,53 +13,67 @@ NotificationServer::~NotificationServer()
 void NotificationServer::start()
 {
     // makes worker start
-    started = true;
 }
 
 OutputCodes NotificationServer::send(Notification message)
 {
     // enqueues the notifications
-
-    // getting notification level makes it usable also in run-time error debugging contexts
-    // if a notification doesnt meet the level configured in the class, it wont be published.
-
+    // writer, unique lock
+    
     if (message.level < minimum_notification_level)
         return OK;
 
-    std::unique_lock<std::mutex> lock(worker_mutex);
-    lock.lock();
+    std::unique_lock lock(worker_mutex);
     notification_queue.push(message);
-    work_todo.notify_all();
     lock.unlock();
 
     return OK;
 }
 
+OutputCodes NotificationServer::sendQueue(std::queue<Notification>* messages_queue)
+{   
+    // writer, unique lock
+    std::unique_lock lock(worker_mutex);
+    while (messages_queue->size())
+    {
+        notification_queue.push(messages_queue->front());
+        messages_queue->pop();
+    }
+    lock.unlock();
+    return OK;
+}
+
 OutputCodes NotificationServer::do_work()
 {
-    // lock queue
-    // send notifications
-    // unlock queue
-
-    std::unique_lock lock(worker_mutex);
-    work_todo.wait(lock);
+    // reader, shared lock
+    std::shared_lock lock(worker_mutex);
     
-    if (started)
+    try
     {
-        // make condition variable that notifies if there are any messages waiting to resolution
-        std::unique_lock<std::mutex> worker_mutex;
-        
-        while (notification_queue.size())
+        if (started)
         {
-            if (!log_file_path.empty())
-                write_to_log(notification_queue.front());
+            // make condition variable that notifies if there are any messages waiting to resolution
+            std::unique_lock<std::mutex> worker_mutex;
             
-            if (!mqtt_address.empty())
-                write_to_mqtt(notification_queue.front());
-        }
-        
-        worker_mutex.unlock();
-    }   
+            while (notification_queue.size())
+            {
+                if (!log_file_path.empty())
+                    write_to_log(notification_queue.front());
+                
+                if (!mqtt_address.empty())
+                    write_to_mqtt(notification_queue.front());
+                
+                notification_queue.pop();
+            }
+        }   
+        lock.unlock();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        lock.unlock();
+        return WARNING;
+    }
     return OK;
 }
 
@@ -72,4 +87,14 @@ OutputCodes write_to_mqtt(Notification message)
 {
     // write to mqtt channel
     return OK;
+}
+
+void NotificationServer::setLogFilePath(std::string log_file)
+{
+    log_file_path = log_file;
+}
+
+void NotificationServer::setMqttAddress(std::string mqtt)
+{
+    mqtt_address = mqtt;
 }
